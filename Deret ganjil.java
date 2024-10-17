21967d4d1326c95ab3674bd09d9057813c371c70
Nasywanau25 <nasywanauraasila@gmail.com>
2024-10-17 19:22:20 +0700

    Create Deret ganjil.java

---
+++ Deret ganjil.java
@@ -0,0 +1,29 @@
+package Pertemuan_4;
+
+public class DeretGanjil {
+    public static void main(String[] args) {
+
+        // //while
+        // int i = 1;
+        // int j = 0;
+        // while (i<=37) {
+        // System.out.print(i + " ");
+        // i += j;
+        // j++;
+        // }
+
+        // for
+        int t = 3;
+        for (int h = 0; h <= 135;) {
+            System.out.print(h + " ");
+            h += t;
+            t += 3;
+        }
+
+        // // cara bapak
+        // int angka = 3, penambah;
+        // for (penambah=0; penambah<=8; penambah++){
+        // System.out.print(angka + " ");
+        // angka+=penambah;
+        // }
+    }
